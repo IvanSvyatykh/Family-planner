262d3a415f521b825b8baaa59fdb532b5d94a4d7
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-02-08 19:11:50 +0500

    EmailSender

---
+++ Project programming/EmailWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_programming
+{
+    public class EmailWriter
+    {
+        private static string Password = "fieAHud2xqR4AxckyWMw";// пароль отправителя
+        private static string Email = "family.planner@mail.ru";// адреса отправителя      
+        static void SendMessage(string adressTo, string messageSubject, string messageText)
+        {
+            try
+            {                        
+                MailMessage mess = new MailMessage();
+                mess.To.Add(adressTo); // адрес получателя
+                mess.From = new MailAddress(Email);
+                mess.Subject = messageSubject; // тема
+                mess.Body = messageText; // текст сообщения
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.mail.ru"; //smtp-сервер отправителя
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(Email, Password);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mess); // отправка пользователю              
+                mess.Dispose();
+                return;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Mail.Send: " + e.Message);
+            }
+        }
+
+    }
+}
