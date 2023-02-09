e0ef194ff7ea45c5370df94f9c4a1c2f1d4b9989
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-02-09 10:06:34 +0500

    Добавил  вывод окон в ForgottenPage

--- Project programming/ForgottenPasswordPage.xaml.cs
+++ Project programming/ForgottenPasswordPage.xaml.cs
@@ -1,18 +1,30 @@
 using WorkWithEmail;
+using PasswordLogic;
 
 namespace Project_programming;
 public partial class ForgottenPasswordPage : ContentPage
 {
     private string Email;
+    private DateTime Time;
     public ForgottenPasswordPage()
     {
         InitializeComponent();
     }
     private void NameEntry_Email(object sender, TextChangedEventArgs e) => Email = emailEntry.Text;
 
-    private void SendEmail(object sender, EventArgs e)
+    private async void SendEmail(object sender, EventArgs e)
     {
-        EmailWriter.SendMessage(Email, "Ex", "Hello");
-    }
 
+        int Ñonfirmationcode = PasswordLog.RandomNumberGenerator();
+        EmailWriter.SendMessage(Email, "Ñonfirmation code", "Code: " + Ñonfirmationcode.ToString());
+
+        Dictionary<int, string[]> messages = new Dictionary<int, string[]>
+        {
+            { 0, new string[] { "", "We have alredy sent you messege", "Ok" } },
+            { 1, new string[] { } },
+            { 2, new string[] {"Attention", "Check code correctness", "Ok"}},
+            { 3, new string[] { "Great", "You can restore your password", "Ok" } },
+            { 4, new string[] {}}
+        };
+    }
 }
