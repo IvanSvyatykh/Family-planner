8e21ab423f38bef2ae6557bbac42b00a2f0bd065
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-03-03 22:02:29 +0500

    .

--- Project programming/Account/AccountPageViewModel.cs
+++ Project programming/Account/AccountPageViewModel.cs
@@ -4,16 +4,42 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AccountViewModel
 {
     public class AccountPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand ShowParametersCommand => new Command(ChangeShowParameters);
 
+        private bool _showParametr = true;
         public AccountPageViewModel() 
         {
 
+
+
+            
+
+
+
+
+
+
+        }
+        public bool ShowParameters
+        {
+            get => _showParametr; 
+            set
+            {
+                if (value == _showParametr) return;
+                _showParametr = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ShowParameters"));
+            }
+        }
+        public void ChangeShowParameters()
+        {
+            _showParametr = !_showParametr;
         }
 
 
