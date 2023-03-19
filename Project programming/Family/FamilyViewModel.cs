011625a2239901dd58f3521f83abf4585607bc5e
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-03-19 16:08:35 +0500

    Создал поля для FamilyPage

--- Project programming/Family/FamilyViewModel.cs
+++ Project programming/Family/FamilyViewModel.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using WorkWithDatabase;
 using Classes;
+using System.Runtime.CompilerServices;
 
 namespace FamilyPage
 {
@@ -16,26 +17,55 @@ namespace FamilyPage
     {
         public event PropertyChangedEventHandler PropertyChanged;
         private static User _user = DatabaseLogic.GetFullPersonInformation((App.Current as App).UserEmail);
-        private bool _isExistFamily = IsFamilyIdEmpty();
-        public ICommand RefreshPage;
-        private bool _isRefreshing = false;
+        private bool _isFamilyIdEmpty = IsFamilyIdEmpty();
+        public ICommand CreateFamily;
+        private string _familyName = null;
+        private string _familyCode = null;
 
 
         public FamilyViewModel()
         {
-            RefreshPage = new Command(() =>
+            CreateFamily = new Command(async () =>
             {
-               
-              
-            });
+
+
+            },
+             () => FamilyName != null && FamilyCode != null);
 
 
 
         }
 
         private static bool IsFamilyIdEmpty() => _user.FamilyId == null;
-        public bool IsRefreshing => _isRefreshing;
-        public bool IsExistFamily => _isExistFamily;
-        public bool FamilyExist => !_isExistFamily;
+        public bool IsFamilyEmpty => _isFamilyIdEmpty;
+        public string FamilyName
+        {
+            get => _familyName;
+            set
+            {
+                if (_familyName != value)
+                {
+                    _familyName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string FamilyCode
+        {
+            get => _familyCode;
+            set
+            {
+                if (_familyCode != value)
+                {
+                    _familyCode = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            ((Command)CreateFamily).ChangeCanExecute();
+        }
     }
 }
