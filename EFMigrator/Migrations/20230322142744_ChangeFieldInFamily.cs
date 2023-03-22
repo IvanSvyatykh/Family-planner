cf1ca0d4107d2812cc899e2b367ca9656862520d
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-03-22 19:28:22 +0500

    Поменял имя поля

---
+++ EFMigrator/Migrations/20230322142744_ChangeFieldInFamily.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFMigrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFieldInFamily : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "Families",
+                newName: "Password");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Families",
+                newName: "Code");
+        }
+    }
+}
